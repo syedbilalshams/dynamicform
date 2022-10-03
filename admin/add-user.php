2139b709ff69ae85ebca56e708641169fc1cd77a
khalid-aptech <khalid@aptechnorth.edu.pk>
2022-10-03 10:41:47 +0500

    Work on adding user

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,8 +1,8 @@
 <head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
 
@@ -44,6 +44,37 @@
                     <input type="submit" name="save" class="btn btn-primary" value="Save" required />
                 </form>
                 <!-- Form End-->
+
+
+                <?php
+                if (isset($_POST["save"])) {
+                    $user_fname = $_POST["fname"];
+                    $user_lname = $_POST["lname"];
+                    $user_name = $_POST["user"];
+                    $user_password = md5($_POST["password"]);
+                    $user_role = $_POST["role"];
+
+                    include "config.php";
+
+                    $query = "SELECT * FROM `user` WHERE username = '{$user_name}'";
+
+                    $result  = mysqli_query($conn, $query);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        echo "user alraedy exist";
+                    } else {
+                        $query1 = "INSERT INTO `user`(`first_name`, `last_name`, `username`, `password`, `role`) VALUES ('{$user_fname}','{$user_lname}','{$user_name}','{$user_password}','{$user_role}')";
+
+                        mysqli_query($conn, $query1);
+
+                        header("Location:http://localhost:82/kj/admin/users.php");
+                    }
+                }
+
+
+                ?>
+
+
             </div>
         </div>
     </div>
