0a5128b2988e7ebf14cfca0851cb845f3361b1f3
syedbilalshams <bilalshams967@gmail.com>
2022-10-15 20:55:20 +0500

    baqi kaam karna hai

--- admin/add-products.php
+++ admin/add-products.php
@@ -19,7 +19,19 @@
                       <div class="form-group">
                           <label for="exampleInputPassword1">Category</label>
                           <select name="category" class="form-control">
-                              <option value="" selected> Select Category</option>
+                              <option value="" disabled> Select Category</option>
+                              <?PHP 
+                               include "config.php";
+                               $query = "SELECT * FROM `category`";
+                               $result = mysqli_query($conn,$query);
+                               if(mysqli_num_rows($result)){
+                                   while($row = mysqli_fetch_assoc($result)){
+                                      echo "<option value=''>{$row['category_name']}</option>";
+                                   }
+                               }
+                              
+                              
+                              ?>
                           </select>
                       </div>
                       <div class="form-group">
